169380f72c036850613f714483b4a0c5ba303aec
ChrisSun0731 <chris20090731@gmail.com>
2026-07-18 22:08:12 +0800

    Update firebase Hosting configuration and dependencies

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/compat/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCauuEAEEmDm2_fOOZZSImrviHsxZxdPqM',
-  authDomain: 'appointed-store-official.firebaseapp.com',
-  projectId: 'appointed-store-official',
-  storageBucket: 'appointed-store-official.firebasestorage.app',
-  messagingSenderId: '666478925068',
-  appId: '1:666478925068:web:c79bd8a77499ca02e62529',
-  measurementId: 'G-NH92VNQZ8H',
+  apiKey: 'AIzaSyAta13HLgDPlIqHrSyO7TXuEmKM5hqHy7w',
+  authDomain: 'ck-tfg-promo.firebaseapp.com',
+  projectId: 'ck-tfg-promo',
+  storageBucket: 'ck-tfg-promo.firebasestorage.app',
+  messagingSenderId: '822381626331',
+  appId: '1:822381626331:web:3c629cb9cac3f8d040ddfe',
+  measurementId: 'G-N1P86S2FL9',
 };
 
 firebase.initializeApp(firebaseConfig);
